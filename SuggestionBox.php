54777ae41768d46341360d24b53723f0ec90cbc8
rafaelfaustini <contato@rafaelfaustini.com.br>
2019-04-15 17:47:10 -0300

    Form creation

--- SuggestionBox.php
+++ SuggestionBox.php
@@ -2,6 +2,7 @@
 require Config.php;
 
 class SuggestionBox {
+  private $name;
   private $text;
   private $classes= Array();
   private $config;
@@ -11,11 +12,37 @@ class SuggestionBox {
       $this->$config = new Config();
   }
 
+  private function send(){
+    // Sends suggestion to the config email
+
+  }
+
+  private function checkPOST(){
+    $content =$_POST[$this->$name];
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') { // Works like a POST listener
+      if(isset($content)){
+        $this->send();
+      }
+    }
+  }
+
+  private function createForm($input){
+    $open = "<form class";
+  }
+
   private function setHtml(){
     $classes = $this->config->getClasses();
-    $str = "<textarea class='$classes'></textarea>";
+    $input = "<textarea class='$classes' name="$this->$name"></textarea>";
+
+    $this->createForm($input);
+
   }
-  public function __construct (  ) {
+
+  private function setName($name){
+    $this->$name = $name;
+  }
+  public function __construct ( $name ) {
+    $this->$setName($name);
 
   }
 
